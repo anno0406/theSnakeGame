be21b6d3e9efde673e093e68cdf33e294a18eaf3
Marie Backman <marie.backman@gmail.com>
2017-10-18 16:46:46 +0200

    highscore fix

--- src/com/company/Snake.java
+++ src/com/company/Snake.java
@@ -15,7 +15,7 @@ public class Snake implements Draw {
     public Snake() {
         this.direction = Direction.RIGHT;
         for (int i = 0; i < 5; i++) {
-            points.add(new Point(Main.XSTART + Main.WIDTH/2 + i, Main.YSTART + Main.HEIGHT/2));
+            points.add(new Point(Main.XSTART + Main.WIDTH / 2 + i, Main.YSTART + Main.HEIGHT / 2));
         }
     }
 
@@ -32,34 +32,51 @@ public class Snake implements Draw {
         }
     }
 
-    public boolean pearSpot(Pear pear){
-        int xPos = points.get(points.size()-1).x;
-        int yPos = points.get(points.size()-1).y;
-        if (pear.p.x == xPos && pear.p.y == yPos){
+    public boolean pearSpot(Pear pear) {
+        int xPos = points.get(points.size() - 1).x;
+        int yPos = points.get(points.size() - 1).y;
+        if (pear.p.x == xPos && pear.p.y == yPos) {
             return true;
         }
         return false;
     }
 
-    public void move(Key key, Pear pear, Terminal terminal){
-        int posXForLastElem = points.get(points.size()-1).x;
-        int posYForLastElem = points.get(points.size()-1).y;
-        if (!pearSpot(pear)){
+    public boolean superPearSpot(SuperPear pear) {
+        int xPos = points.get(points.size() - 1).x;
+        int yPos = points.get(points.size() - 1).y;
+        if (pear.p.x == xPos && pear.p.y == yPos) {
+            return true;
+        }
+        return false;
+    }
+
+    public void move(Key key, Pear pear, Terminal terminal, SuperPear superPear) throws InterruptedException {
+        int posXForLastElem = points.get(points.size() - 1).x;
+        int posYForLastElem = points.get(points.size() - 1).y;
+        if (!pearSpot(pear) && !superPearSpot(superPear)) {
             terminal.moveCursor(points.get(0).x, points.get(0).y);
             terminal.putCharacter(' ');
             points.remove(0);
         }
-        else {
+        else if (pearSpot(pear)) {
             pear.put();
             scoreCounter += 1000;
-        }
-
-        if (key == null){
-            moveNoKeyPressed(posXForLastElem, posYForLastElem);
+            if (scoreCounter % 5000 == 0) {
+                superPear.put();
+            }
+            Main.speedUp();
         }
         else {
+            scoreCounter += 5000;
+            superPear.moveOutside();
+            Main.speedUp();
+        }
+
+        if (key == null) {
+            moveNoKeyPressed(posXForLastElem, posYForLastElem);
+        } else {
             Key.Kind kbd = key.getKind();
-            if (direction == Direction.UP || direction == Direction.DOWN){
+            if (direction == Direction.UP || direction == Direction.DOWN) {
                 switch (kbd) {
                     case ArrowLeft:
                         moveLeft(posXForLastElem, posYForLastElem);
@@ -75,8 +92,7 @@ public class Snake implements Draw {
                         moveNoKeyPressed(posXForLastElem, posYForLastElem);
                         break;
                 }
-            }
-            else {
+            } else {
                 switch (kbd) {
                     case ArrowUp:
                         moveUp(posXForLastElem, posYForLastElem);
@@ -95,15 +111,17 @@ public class Snake implements Draw {
         }
     }
 
-    private void moveNoKeyPressed(int posX, int posY) {
+    private void moveNoKeyPressed(int posX, int posY) throws InterruptedException {
 
         switch (direction) {
             case UP:
                 moveUp(posX, posY);
+                Thread.sleep(40);
                 break;
 
             case DOWN:
                 moveDown(posX, posY);
+                Thread.sleep(40);
                 break;
 
             case LEFT:
@@ -118,31 +136,31 @@ public class Snake implements Draw {
 
 
     private void moveUp(int posX, int posY) {
-        points.add(new Point(posX, posY-1));
+        points.add(new Point(posX, posY - 1));
     }
 
     private void moveDown(int posX, int posY) {
-        points.add(new Point(posX, posY+1));
+        points.add(new Point(posX, posY + 1));
     }
 
     private void moveLeft(int posX, int posY) {
-        points.add(new Point(posX-1, posY));
+        points.add(new Point(posX - 1, posY));
     }
 
     private void moveRight(int posX, int posY) {
-        points.add(new Point(posX+1, posY));
+        points.add(new Point(posX + 1, posY));
     }
 
-    public boolean isAlive(){
+    public boolean isAlive() {
         for (int i = 0; i < points.size() - 3; i++) {
-            if (points.get(i).x == points.get(points.size()-1).x &&
-                    points.get(i).y == points.get(points.size()-1).y ){
+            if (points.get(i).x == points.get(points.size() - 1).x &&
+                    points.get(i).y == points.get(points.size() - 1).y) {
                 return false;
             }
         }
 
         for (Point elem : Main.cornerPoints) {
-            if (points.get(points.size()-1).x == elem.x && points.get(points.size()-1).y == elem.y){
+            if (points.get(points.size() - 1).x == elem.x && points.get(points.size() - 1).y == elem.y) {
                 return false;
             }
         }
